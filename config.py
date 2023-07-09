2403d86d48da4a22c0079f05fbe81ce3e688fb30
Daniel Ebert <iuq@web.de>
2023-07-09 04:29:28 -0700

    working v1

---
+++ config.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import Any
+
+
+class Config:
+    def __init__(self):
+        self.config = {}
+
+    def add(self, key: str, value: Any) -> None:
+        self.config[key] = value
+
+    def __getattr__(self, name: str) -> Any:
+        if name not in self.config:
+            raise AttributeError(f'Settings object has no attribute {name}')
+        return self.config[name]
+
+    def __repr__(self) -> str:
+        return 'Config:\n' + '\n'.join([f'    {key}: {value}' for (key, value) in self.config.items()])
+
+    def __str__(self) -> str:
+        return str(self.config)
+
+
+settings = Config()
---
+++ main.py
@@ -0,0 +1,9 @@
+
+from config import settings
+
+settings.add('repository_root_path', '123')
+
+print(settings.repository_root_path)
+
+print(repr(settings))
+print(settings)
